c07b00267ef08e6fea04b8b5303b32e5581406bd
Max Henrique <maxhenrique308@gmail.com>
2026-05-18 12:28:35 -0300

    logo e favicon

--- views/components/header.php
+++ views/components/header.php
@@ -86,6 +86,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Câmara Municipal de Perdões - MG</title>
     <link rel="stylesheet" href="/styles.css">
+    <link rel="icon" href="/img/favicon.png"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 <body>
